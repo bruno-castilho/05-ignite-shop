eabebd7ecaf469e40d771f3f10f991beb4063aec
Bruno <bruno.silva.castilho@gmail.com>
2024-03-08 15:29:31 -0300

    Carregando dados do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,3 +1,4 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/product"
 import { useRouter } from "next/router"
 
 
@@ -6,6 +7,18 @@ export default function Product(){
 
 
     return (
-        <h1>{JSON.stringify(query)}</h1>
+        <ProductContainer>
+            <ImageContainer>
+                
+            </ImageContainer>
+            <ProductDetails>
+                <h1>Camiseta X</h1>
+                <span>R$ 79.90</span>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam harum cumque consequuntur neque doloremque enim vel, aut nostrum ad dicta vitae rerum fugit laboriosam eum temporibus dolores exercitationem velit minima?</p>
+                <button>
+                    Comprar Agora
+                </button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
